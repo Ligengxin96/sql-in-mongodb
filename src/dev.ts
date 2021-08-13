8af4f203177176af8a5aec3e4647d18c9b4e08c3
Ligengxin <Ligengxin96@gmail.com>
2021-08-13 21:38:43 +0800

    update

--- src/dev.ts
+++ src/dev.ts
@@ -1,18 +1,14 @@
 import { SQLParser } from './index';
 
-const sqlWhereConditon = `
-select * from t WHERE title >= 123 or title <= 234 
-or title in ('1', '2') or id between 1 and 2
-or title = '123' or title is not null
-`;
+const sqlWhereConditon = `select t1.*, t2.* from t1, t2`;
 const parser = new SQLParser({multipleLineSql: true});
 const mongoQuery = parser.parseSql(sqlWhereConditon);
 const selectedColumns = parser.getSelectedFeilds(sqlWhereConditon);
-
+const tableNmae = parser.getSelectedTable(sqlWhereConditon);
 
 console.log(selectedColumns);
 console.log(`db.test.find(${JSON.stringify(mongoQuery)})`);
-console.log(mongoQuery);
+console.log(tableNmae);
 
 // db.test.find({$and:[{"a":{$gt:"undefined"}},{"name":"123"}]})
 // db.postmessages.find({ $expr: { "$eq": [{ "$dateFromString": { "dateString": "2021-06-25T08:00:47.134Z" }}, "$createdTime" ]} })
